fce21cbc5d380201c474a931b2167ab37d68ef66
Jeff Wyckoff <jokerswild663@gmail.com>
2025-06-16 15:56:11 -0700

    excercise 2 target circle count elements

--- features/steps/target_circle.py
+++ features/steps/target_circle.py
@@ -6,3 +6,9 @@ from time import sleep
 def open_page(context):
   context.driver.get('https://target.com/circle')
   sleep(5)
+
+@then("verify at least 10 benefits")
+def verify_benefits_cells(context):
+  expected=10
+  benefit_cells=context.driver.find_elements(By.CSS_SELECTOR,"div[class*=sc-acea07d2]")
+  assert len(benefit_cells)>=expected, f"actualength: {len(benefit_cells)}, expected: {expected}"
--- features/tests/target_circle.feature
+++ features/tests/target_circle.feature
@@ -1,6 +1,7 @@
 # Created by joker at 6/15/2025
-Feature: # Enter feature name here
-  # Enter feature description here
+Feature: Testing Target circle Elements
+  testing target circle elements
 
-  Scenario: # Enter scenario name here
-    # Enter steps here
+  Scenario: Target Circle Benefit Cells
+    Given open target circle
+    Then verify at least 10 benefits
