572bc2a711e09371b8ca780c06dcf23bb2ff2a8f
Jeff Wyckoff <jokerswild663@gmail.com>
2025-06-18 16:08:53 -0700

    Excercise 3 add target product to cart

--- features/steps/cart.py
+++ features/steps/cart.py
@@ -7,9 +7,30 @@ def click_cart(context,cart_icon):
     context.driver.find_element(By.CSS_SELECTOR,"div[data-test='@web/CartIcon'] svg").click()
 
 
+@when("click add to cart from search")
+def click_add_to_cart_search(context):
+    context.driver.execute_script("window.scrollTo(0, 1000)")
+    context.driver.find_element(By.CSS_SELECTOR, "div[style='display: inline-block;'] button[class*='styles_ndsBaseButton']").click()
+    context.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+
+@when("click add to cart panel")
+def click_add_to_cart_panel(context):
+  context.driver.find_element(By.CSS_SELECTOR, "button[data-test='orderPickupButton']").click()
+  sleep(5)
+
+
+@then("verify {item} in cart")
+def verify_in_cart(context,item):
+    actual=context.driver.find_element(By.CSS_SELECTOR,"h2[data-test='modal-drawer-heading'] span.h-text-lg").text
+    expected="Added to cart"
+    assert actual==expected, f"actual: {actual} not equal expected: {expected}"
+
+
 @then("verify {object} empty")
 def cart_empty(context,object):
-    expected='Your cart is empty'
-    actual=context.driver.find_element(By.CSS_SELECTOR, "div[data-test='boxEmptyMsg'] h1[class*='ndsHeading']").text
+    products_in_cart=context.driver.find_elements(By.CSS_SELECTOR, "div.product-image img")
     sleep(5)
-    assert actual==expected, f"actual: '{actual}' does not equal expected: '{expected}'"
+    assert len(products_in_cart) > 0, "no products in cart"
+
+
--- features/tests/cart.feature
+++ features/tests/cart.feature
@@ -6,3 +6,10 @@ Feature: testing target cart
     Given open target page target.com
     When click cart icon
     Then verify cart empty
+
+  Scenario: Add product to Cart
+    Given open target page target.com
+    When target search tea
+    And click add to cart from search
+    And click add to cart panel
+    Then verify tea in cart
