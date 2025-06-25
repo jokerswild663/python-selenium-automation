0f234ae3ecb648c46ce8e93a74952d28f0f668d3
Jeff Wyckoff <jokerswild663@gmail.com>
2025-06-25 14:27:00 -0700

    Modifying target signin for page object model

--- features/steps/target_signin.py
+++ features/steps/target_signin.py
@@ -3,23 +3,16 @@ from behave import given, when, then
 from time import sleep
 from selenium.webdriver.support import expected_conditions as EC
 
-
-ACCOUNT_ICON=(By.CSS_SELECTOR, "a[aria-label='Account, sign in']")
-ACCOUNT_ICON_PANEL=(By.CSS_SELECTOR, "button[data-test='accountNav-signIn']")
-ACCOUNT_SIGN_IN=(By.CSS_SELECTOR, "div[tabindex='-1'] h1.h-margin-b-tiny")
-
 @when("click account {dropdown}")
 def click_account(context,dropdown):
-    context.driver.wait.until(EC.element_to_be_clickable(ACCOUNT_ICON)).click()
+    context.app.sign_in.click_sign_in()
 
 
 @when("click sign in {button}")
 def click_sign_in(context,button):
-    context.driver.wait.until(EC.element_to_be_clickable(ACCOUNT_ICON_PANEL)).click()
-
+    context.app.sign_in.click_sign_in_panel()
 
 @then("verify {page} loaded")
 def sign_in_page_loaded(context,page):
     expected='Sign in or create account'
-    actual=context.driver.wait.until(EC.visibility_of_element_located(ACCOUNT_SIGN_IN)).text
-    assert actual==expected, f"actual: '{actual}' does not match expected: '{expected}'"
+    context.app.sign_in.verify_sign_in(expected)
