ee906a5df3e73f71443e5a22c8518f2afc3fe070
Jeff Wyckoff <jokerswild663@gmail.com>
2025-07-06 23:14:46 -0700

    updating files for homework 9

--- app/application.py
+++ app/application.py
@@ -1,4 +1,5 @@
 from pages.base_page import Page
+from pages.help_page import HelpPage
 from pages.main_page import MainPage
 from pages.header import Header
 from pages.search_results import SearchResultsPage
@@ -16,3 +17,4 @@ class Application:
         self.cart = Cart(driver)
         self.sign_in = SignIn(driver)
         self.terms_conditions = TermsConditions(driver)
+        self.help_page = HelpPage(driver)
--- pages/sign_in.py
+++ pages/sign_in.py
@@ -6,6 +6,11 @@ class SignIn(Page):
     ACCOUNT_ICON = (By.CSS_SELECTOR, "a[aria-label='Account, sign in']")
     ACCOUNT_ICON_PANEL = (By.CSS_SELECTOR, "button[data-test='accountNav-signIn']")
     ACCOUNT_SIGN_IN = (By.CSS_SELECTOR, "div[tabindex='-1'] h1.h-margin-b-tiny")
+    ACCOUNT_USERNAME_FIELD = (By.CSS_SELECTOR, "input[name='username']")
+    ACCOUNT_PASSWORD_FIELD = (By.CSS_SELECTOR, "input[name='password']")
+    ACCOUNT_USERNAME_CONTINUE_BUTTON = (By.CSS_SELECTOR, "button[id='login']")
+    ACCOUNT_PASSWORD_SUBMIT_BUTTON = (By.CSS_SELECTOR, "button[id='login'][type='submit']")
+    ACCOUNT_PASSWORD_INCORRECT_MESSAGE = (By.CSS_SELECTOR, "span[style*='--error-message']")
 
     def click_sign_in(self):
         self.click_element(*self.ACCOUNT_ICON)
@@ -17,3 +22,14 @@ class SignIn(Page):
     def verify_sign_in(self,expected):
         # actual=self.driver.find_element(*self.ACCOUNT_SIGN_IN)
         actual=self.verify_text(expected,self.ACCOUNT_SIGN_IN)
+
+    def enter_username(self):
+        self.input_text('jokerswild663@gmail.com',*self.ACCOUNT_USERNAME_FIELD)
+        self.click_element(*self.ACCOUNT_USERNAME_CONTINUE_BUTTON)
+
+    def enter_wrong_password(self):
+        self.input_text('blargg',*self.ACCOUNT_PASSWORD_FIELD)
+        self.click_element(*self.ACCOUNT_PASSWORD_SUBMIT_BUTTON)
+
+    def verify_wrong_password(self,expected):
+        self.verify_text(expected,self.ACCOUNT_PASSWORD_INCORRECT_MESSAGE)
